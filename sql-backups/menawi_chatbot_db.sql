b327182dc6c41b37957f57bee886a31d949791e0
Janzen Faidiban <janzenfaidiban@gmail.com>
2025-05-18 17:15:05 +0900

    added: sql backups

---
+++ sql-backups/menawi_chatbot_db.sql
@@ -0,0 +1,44 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+DROP TABLE IF EXISTS `chat_history`;
+CREATE TABLE `chat_history` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_message` text NOT NULL,
+  `bot_response` text NOT NULL,
+  `created_at` timestamp NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=85 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+INSERT INTO `chat_history` (`id`, `user_message`, `bot_response`, `created_at`) VALUES
+(81, 'halo', 'Halo! Selamat datang di chatbot belajar Bahasa Menawi.', '2025-05-18 16:22:15'),
+(82, 'perkenalkan diri anda', 'Saya ChatBot AI Belajar Bahasa Menawi, senang membantu Anda belajar Bahasa Menawi (Papua). Silakan bertanya, saya dengan senang hati membantu!', '2025-05-18 16:22:30'),
+(83, 'ayah', 'Dalam Bahasa Menawi, \"ayah\" adalah \"Dai\".', '2025-05-18 16:22:38'),
+(84, 'ibu', '\"Ibu\" dalam Bahasa Menawi adalah \"Mane\".', '2025-05-18 16:22:41');
+INSERT INTO `users` (`id`, `username`, `password`, `created_at`) VALUES
+(1, 'admin', '$2y$10$.wDqiT6t3otRHkYMyqD3RO0dISmY4ab4HMbS8nG20LxP0AZE1qaYG', '2025-05-18 17:03:20');
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
